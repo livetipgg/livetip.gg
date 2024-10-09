ac1cf5e6c07d887e60324725a1fa2c7822644815
dumoresco <eduardomorescoiost@gmail.com>
2024-10-09 20:40:29 -0300

    Fix build config

--- src/components/no-content.tsx
+++ src/components/no-content.tsx
@@ -1,12 +1,17 @@
+import { Info } from "lucide-react";
+
 interface NoContentProps {
   message?: string;
 }
 
 export const NoContent = ({ message }: NoContentProps) => {
   return (
-    <div className="flex flex-col items-center justify-center bg-background border border-input rounded p-2 ">
-      <div className="text-sm font-medium text-gray-500">
-        {message || " Nenhum conteúdo disponível"}
+    <div className="flex flex-col items-center justify-center  border border-input rounded p-2 bg-muted/40 ">
+      <div className=" text-gray-500 flex items-center gap-2">
+        <Info className="h-6 w-6 text-gray-500" />
+        <span className="text-sm font-medium p-0 m-0">
+          {message || " Nenhum conteúdo disponível"}
+        </span>
       </div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      template: "index.html",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {},
+  base: "/",
 });
