56c203377490b2c96fdc693a2b193724efec2927
dumoresco <eduardomorescoiost@gmail.com>
2024-10-24 20:44:07 -0300

    fix provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import WebSocketProvider from "./config/WebSocketProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // <WebSocketProvider>
-  <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-    <App />
-  </ThemeProvider>
-  // </WebSocketProvider>
+  <WebSocketProvider>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </WebSocketProvider>
 );
