2c0e2f91bb122eb6888ddaad24767775569e66e5
dumoresco <eduardomorescoiost@gmail.com>
2024-11-22 17:01:29 -0300

    ajustes

--- src/features/transmission/presentations/index.tsx
+++ src/features/transmission/presentations/index.tsx
@@ -228,151 +228,155 @@ const TransmissionPage = () => {
               )}
 
               {!!transmissionMessages.results.length &&
-                transmissionMessages.results.map((message) => (
-                  <>
-                    <div className="flex items-center justify-between mt-5">
-                      <div className="flex items-center ">
-                        <div
-                          className={`${
-                            message.read ? "bg-gray-500" : "bg-success"
-                          } p-2 rounded-s-lg `}
-                        >
-                          <span className="text-white font-semibold">
-                            {formatPayment({
-                              amount: message.amount,
-                              type: message.currency,
-                            })}
-                          </span>
+                [...transmissionMessages.results] // Cria uma cópia do array original
+                  .sort((a, b) => Number(a.read) - Number(b.read)) // Ordena pela propriedade `read`, colocando `true` primeiro
+                  .map((message) => (
+                    <>
+                      <div className="flex items-center justify-between mt-5">
+                        <div className="flex items-center ">
+                          <div
+                            className={`${
+                              message.read ? "bg-gray-500" : "bg-success"
+                            } p-2 rounded-s-lg `}
+                          >
+                            <span className="text-white font-semibold">
+                              {formatPayment({
+                                amount: message.amount,
+                                type: message.currency,
+                              })}
+                            </span>
+                          </div>
+                          <div
+                            className={`p-2 ${
+                              message.read ? "bg-gray-400" : "bg-primary"
+                            } rounded-e-lg`}
+                          >
+                            <span className="font-semibold text-white">
+                              {message.sender}
+                            </span>
+                          </div>
+                          <div className="w-10 h-10 ml-2 bg-card-custom border p-1 rounded-md  -top-4 ">
+                            <PaymentIcon currency={message.currency} />
+                          </div>
                         </div>
-                        <div
-                          className={`p-2 ${
-                            message.read ? "bg-gray-400" : "bg-primary"
-                          } rounded-e-lg`}
-                        >
-                          <span className="font-semibold text-white">
-                            {message.sender}
-                          </span>
-                        </div>
-                        <div className="w-10 h-10 ml-2 bg-card-custom border p-1 rounded-md  -top-4 ">
-                          <PaymentIcon currency={message.currency} />
-                        </div>
-                      </div>
-                      <div className="flex items-center gap-2">
-                        <Button
-                          disabled={isLoadingTransmissionMessages}
-                          onClick={() => {
-                            if (message.read) {
-                              setMessageUnread(message._id);
-                            }
+                        <div className="flex items-center gap-2">
+                          <Button
+                            disabled={isLoadingTransmissionMessages}
+                            onClick={() => {
+                              if (message.read) {
+                                setMessageUnread(message._id);
+                              }
 
-                            if (!message.read) {
-                              setMessageRead(message._id);
-                            }
-                          }}
-                          variant="outline"
-                          size={"icon"}
-                          title={
-                            message.read
-                              ? "Marcar como não lido"
-                              : "Marcar como lido"
-                          }
-                        >
-                          {message.read ? (
-                            <div className="flex items-center gap-2">
-                              <MailX className="w-4 h-4" />
-                            </div>
-                          ) : (
-                            <MailCheck className="w-4 h-4" />
-                          )}
-                        </Button>
-                        {!message.read && (
-                          <AlertDialog
-                            onOpenChange={(open) => {
-                              if (!open) {
-                                if (message.read) return;
+                              if (!message.read) {
                                 setMessageRead(message._id);
                               }
                             }}
+                            variant="outline"
+                            size={"icon"}
+                            title={
+                              message.read
+                                ? "Marcar como não lido"
+                                : "Marcar como lido"
+                            }
                           >
-                            <AlertDialogTrigger>
-                              <Button
-                                title="Expandir mensagem"
-                                size={isMobile ? "icon" : "default"}
-                                variant="secondary"
-                                className="flex items-center gap-2 "
-                              >
-                                {!isMobile && "Expandir"}
-                                <ExternalLink className="w-4   h-full" />
-                              </Button>
-                            </AlertDialogTrigger>
-                            <AlertDialogContent>
-                              <div>
-                                {" "}
-                                <div className="flex items-center ">
-                                  <div
-                                    className={`${
-                                      message.read
-                                        ? "bg-gray-500"
-                                        : "bg-success"
-                                    } p-2 rounded-s-lg `}
-                                  >
-                                    <span className="text-white font-semibold">
-                                      {formatPayment({
-                                        amount: message.amount,
-                                        type: message.currency,
-                                      })}
-                                    </span>
-                                  </div>
-                                  <div
-                                    className={`p-2 ${
-                                      message.read
-                                        ? "bg-gray-400"
-                                        : "bg-primary"
-                                    } rounded-e-lg`}
-                                  >
-                                    <span className="font-semibold text-white">
-                                      {message.sender}
-                                    </span>
-                                  </div>
-                                  <div className="w-10 h-10 ml-2 bg-card-custom border p-1 rounded-md  -top-4 ">
-                                    <PaymentIcon currency={message.currency} />
+                            {message.read ? (
+                              <div className="flex items-center gap-2">
+                                <MailX className="w-4 h-4" />
+                              </div>
+                            ) : (
+                              <MailCheck className="w-4 h-4" />
+                            )}
+                          </Button>
+                          {!message.read && (
+                            <AlertDialog
+                              onOpenChange={(open) => {
+                                if (!open) {
+                                  if (message.read) return;
+                                  setMessageRead(message._id);
+                                }
+                              }}
+                            >
+                              <AlertDialogTrigger>
+                                <Button
+                                  title="Expandir mensagem"
+                                  size={isMobile ? "icon" : "default"}
+                                  variant="secondary"
+                                  className="flex items-center gap-2 "
+                                >
+                                  {!isMobile && "Expandir"}
+                                  <ExternalLink className="w-4   h-full" />
+                                </Button>
+                              </AlertDialogTrigger>
+                              <AlertDialogContent>
+                                <div>
+                                  {" "}
+                                  <div className="flex items-center ">
+                                    <div
+                                      className={`${
+                                        message.read
+                                          ? "bg-gray-500"
+                                          : "bg-success"
+                                      } p-2 rounded-s-lg `}
+                                    >
+                                      <span className="text-white font-semibold">
+                                        {formatPayment({
+                                          amount: message.amount,
+                                          type: message.currency,
+                                        })}
+                                      </span>
+                                    </div>
+                                    <div
+                                      className={`p-2 ${
+                                        message.read
+                                          ? "bg-gray-400"
+                                          : "bg-primary"
+                                      } rounded-e-lg`}
+                                    >
+                                      <span className="font-semibold text-white">
+                                        {message.sender}
+                                      </span>
+                                    </div>
+                                    <div className="w-10 h-10 ml-2 bg-card-custom border p-1 rounded-md  -top-4 ">
+                                      <PaymentIcon
+                                        currency={message.currency}
+                                      />
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                              <p className="break-words max-w-full overflow-auto text-lg">
-                                {message.content}
-                              </p>
-                              <AlertDialogFooter>
-                                <AlertDialogCancel
-                                  onClick={() => {
-                                    if (message.read) return;
-                                    setMessageRead(message._id);
-                                  }}
-                                >
-                                  Voltar
-                                </AlertDialogCancel>
-                              </AlertDialogFooter>
-                            </AlertDialogContent>
-                          </AlertDialog>
-                        )}
+                                <p className="break-words max-w-full overflow-auto text-lg">
+                                  {message.content}
+                                </p>
+                                <AlertDialogFooter>
+                                  <AlertDialogCancel
+                                    onClick={() => {
+                                      if (message.read) return;
+                                      setMessageRead(message._id);
+                                    }}
+                                  >
+                                    Voltar
+                                  </AlertDialogCancel>
+                                </AlertDialogFooter>
+                              </AlertDialogContent>
+                            </AlertDialog>
+                          )}
+                        </div>
                       </div>
-                    </div>
-                    <div
-                      className={`rounded-lg border ${
-                        message.read ? "" : "border-primary"
-                      }  mt-2`}
-                    >
-                      <div className="p-2 flex gap-2 h-auto items-start whitespace-normal max-w-full">
-                        <span className="text-muted-foreground text-sm">
-                          {format(message.timestamp, "HH:mm")}
-                        </span>
-                        <p className="break-words max-w-full overflow-auto">
-                          {message.content}
-                        </p>
+                      <div
+                        className={`rounded-lg border ${
+                          message.read ? "" : "border-primary"
+                        }  mt-2`}
+                      >
+                        <div className="p-2 flex gap-2 h-auto items-start whitespace-normal max-w-full">
+                          <span className="text-muted-foreground text-sm">
+                            {format(message.timestamp, "HH:mm")}
+                          </span>
+                          <p className="break-words max-w-full overflow-auto">
+                            {message.content}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  </>
-                ))}
+                    </>
+                  ))}
             </div>
           </div>
         </div>
