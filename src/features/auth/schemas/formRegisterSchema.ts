4ef95063975e2f5a72da9bf3cca29bd8fff8136f
dumoresco <eduardomorescoiost@gmail.com>
2024-12-02 14:00:06 -0300

    fix schema register

--- src/features/auth/schemas/formRegisterSchema.ts
+++ src/features/auth/schemas/formRegisterSchema.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 
 export const formRegisterSchema = z.object({
-  username: z.string().min(1, { message: "O username é obrigatório" }),
-  email: z.string().email({ message: "O email é inválido" }),
-  password: z
+  username: z
     .string()
-    .min(6, {
-      message: "A senha é obrigatória e deve ter pelo menos 6 caracteres",
+    .min(3, {
+      message: "O username é obrigatório e deve ter pelo menos 3 caracteres",
     }),
+  email: z.string().email({ message: "O email é inválido" }),
+  password: z.string().min(6, {
+    message: "A senha é obrigatória e deve ter pelo menos 6 caracteres",
+  }),
   confirmPassword: z
     .string()
     .min(6, { message: "A confirmação da senha é obrigatória" }),
