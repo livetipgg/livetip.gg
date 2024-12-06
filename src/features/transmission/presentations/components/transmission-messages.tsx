46a26d6780c94603c55cf74ee892d50337acf1d7
dumoresco <eduardomorescoiost@gmail.com>
2024-12-06 13:54:44 -0300

    fix ordem transmission messages

--- src/features/transmission/presentations/components/transmission-messages.tsx
+++ src/features/transmission/presentations/components/transmission-messages.tsx
@@ -26,13 +26,13 @@ export const TransmissionMessages = () => {
               {transmissionMessages.results.length}
             </Badge>
           </TabsTrigger>
-          <TabsTrigger value="lidas" className="w-full gap-2">
-            Lidas
-            <Badge variant="destructive">{readMessages.length}</Badge>
-          </TabsTrigger>
           <TabsTrigger value="nao_lidas" className="w-full gap-2">
             Não Lidas
             <Badge variant="destructive">{unreadMessages.length}</Badge>
+          </TabsTrigger>
+          <TabsTrigger value="lidas" className="w-full gap-2">
+            Lidas
+            <Badge variant="destructive">{readMessages.length}</Badge>
           </TabsTrigger>
         </TabsList>
         <TabsContent value="todas" className="pb-10">
@@ -46,15 +46,6 @@ export const TransmissionMessages = () => {
             <NoContent message="Nenhuma mensagem para mostrar" />
           )}
         </TabsContent>
-        <TabsContent value="lidas" className="pb-10">
-          {!!readMessages.length &&
-            readMessages.map((message) => (
-              <TransmissionMessage message={message} key={message._id} />
-            ))}
-          {!readMessages.length && (
-            <NoContent message="Nenhuma mensagem lida" />
-          )}
-        </TabsContent>
         <TabsContent value="nao_lidas" className="pb-10">
           {!!unreadMessages.length &&
             unreadMessages.map((message) => (
@@ -62,6 +53,15 @@ export const TransmissionMessages = () => {
             ))}
           {!unreadMessages.length && (
             <NoContent message="Nenhuma mensagem não lida" />
+          )}
+        </TabsContent>
+        <TabsContent value="lidas" className="pb-10">
+          {!!readMessages.length &&
+            readMessages.map((message) => (
+              <TransmissionMessage message={message} key={message._id} />
+            ))}
+          {!readMessages.length && (
+            <NoContent message="Nenhuma mensagem lida" />
           )}
         </TabsContent>
       </Tabs>
