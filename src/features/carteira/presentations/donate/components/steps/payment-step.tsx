ce783b2207b48fcbb66c79f5128329f5817a4a14
dumoresco <eduardomorescoiost@gmail.com>
2024-12-11 16:36:43 -0300

    fx

--- src/features/carteira/presentations/donate/components/steps/payment-step.tsx
+++ src/features/carteira/presentations/donate/components/steps/payment-step.tsx
@@ -63,7 +63,7 @@ const PaymentStep = () => {
           </span>
         </div>
         <QRCodeSVG
-          className="bg-white p-2"
+          className="bg-white p-1"
           value={controller.qrCode}
           size={280} // Tamanho do QR Code
           imageSettings={{
