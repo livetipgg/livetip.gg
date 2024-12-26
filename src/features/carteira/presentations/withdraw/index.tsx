2fe4aeecb74403d151b39dc5042913edea62f36b
dumoresco <eduardomorescoiost@gmail.com>
2024-12-26 18:34:05 -0300

    liberado saque pix dev

--- src/features/carteira/presentations/withdraw/index.tsx
+++ src/features/carteira/presentations/withdraw/index.tsx
@@ -53,16 +53,14 @@ const Withdraw = () => {
           <Tabs defaultValue="satoshi" className="">
             <TabsList className="mb-5">
               <div className="flex items-center gap-2 ">
-                {isAdmin && (
-                  <TabsTrigger
-                    value="pix"
-                    className="flex items-center gap-2"
-                    onClick={() => setWithdrawType("BRL")}
-                  >
-                    <PaymentIcon currency="BRL" className="w-4" />
-                    Pix
-                  </TabsTrigger>
-                )}
+                <TabsTrigger
+                  value="pix"
+                  className="flex items-center gap-2"
+                  onClick={() => setWithdrawType("BRL")}
+                >
+                  <PaymentIcon currency="BRL" className="w-4" />
+                  Pix
+                </TabsTrigger>
                 <TabsTrigger
                   value="satoshi"
                   className="flex items-center gap-2"
