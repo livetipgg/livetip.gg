0f547c70d800b61fd6acd8b710fe325a2d589759
dumoresco <eduardomorescoiost@gmail.com>
2025-01-20 08:45:01 -0300

    fx

--- src/features/auth/schemas/formRegisterSchema.ts
+++ src/features/auth/schemas/formRegisterSchema.ts
@@ -6,10 +6,12 @@ export const formRegisterSchema = z.object({
     .min(3, {
       message: "O username é obrigatório e deve ter pelo menos 3 caracteres",
     })
+    // não aceita maiúsculas
     .regex(/^[a-zA-Z0-9]+$/, {
       message:
         "O username só pode conter letras, números. Não pode conter espaços, símbolos ou acentos.",
-    }),
+    })
+    .transform((value) => value.toLowerCase()),
   email: z.string().email({ message: "O email é inválido" }),
   password: z.string().min(6, {
     message: "A senha é obrigatória e deve ter pelo menos 6 caracteres",
