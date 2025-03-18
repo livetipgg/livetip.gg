385e9a4435d52e21ca4b2a5f3785229422bb924d
dumoresco <eduardomorescoiost@gmail.com>
2025-03-18 19:13:30 -0300

    validação cpf

--- src/features/carteira/schemas/createBankAccountSchema.ts
+++ src/features/carteira/schemas/createBankAccountSchema.ts
@@ -1,25 +1,46 @@
 import { z } from "zod";
 import { cpf } from "cpf-cnpj-validator";
 
-export const createBankAccountSchema = z.object({
-  fullName: z.string().min(1, { message: "O nome completo é obrigatório" }),
-  bankId: z.string().min(1, { message: "O banco é obrigatório" }),
-  agencyNumber: z.string().min(1, { message: "A agência é obrigatória" }),
+export const createBankAccountSchema = z
+  .object({
+    fullName: z.string().min(1, { message: "O nome completo é obrigatório" }),
+    bankId: z.string().min(1, { message: "O banco é obrigatório" }),
+    agencyNumber: z.string().min(1, { message: "A agência é obrigatória" }),
 
-  accountNumber: z.string().min(1, { message: "A conta é obrigatória" }),
+    accountNumber: z.string().min(1, { message: "A conta é obrigatória" }),
 
-  pixKey: z.string().min(1, { message: "A chave pix é obrigatória" }),
-  pixKeyType: z
-    .string()
-    .min(1, { message: "O tipo da chave pix é obrigatório" }),
-  cpf: z
-    .string()
-    .min(11, "CPF deve ter 11 dígitos")
-    .max(14, "CPF deve ter no máximo 14 caracteres")
-    .refine((value) => cpf.isValid(value), {
-      message: "CPF inválido",
-    }),
-});
+    pixKey: z.string(),
+
+    pixKeyType: z
+      .string()
+      .min(1, { message: "O tipo da chave pix é obrigatório" }),
+
+    cpf: z
+      .string()
+      .min(11, "CPF deve ter 11 dígitos")
+      .max(14, "CPF deve ter no máximo 14 caracteres")
+      .refine((value) => cpf.isValid(value), {
+        message: "CPF inválido",
+      }),
+  })
+  .superRefine((data, error) => {
+    if (!data.pixKey) {
+      return data;
+    }
+
+    if (data.pixKeyType === "CPF") {
+      if (!cpf.isValid(data.pixKey)) {
+        error.addIssue({
+          path: ["pixKey"],
+          message: "CPF inválido",
+          code: "unrecognized_keys",
+          keys: ["pixKey"],
+        });
+      }
+    }
+
+    return data;
+  });
 
 export type IFormCreateBankAccountSchema = z.infer<
   typeof createBankAccountSchema
